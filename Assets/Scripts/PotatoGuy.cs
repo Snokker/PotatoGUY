76e0887d7a78b65b10919e793fd68d3f7dfba203
Lisa <lisagameart@gmail.com>
2018-07-10 17:53:48 +0200

    damn potato guy movement

---
+++ Assets/Scripts/PotatoGuy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoGuy : MonoBehaviour {
+
+    //members
+    public float speed = 1.5f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+     void Update ()
+     {
+        Movement();
+     }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            return;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            return;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+            return;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+            return;
+        }
+    }
+ }
