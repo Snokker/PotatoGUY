d422ff38c5ebded15b7f42891fe1894329874954
Lisa <lisagameart@gmail.com>
2018-07-10 19:00:56 +0200

    potato guy

--- Assets/Scripts/PotatoGuy.cs
+++ Assets/Scripts/PotatoGuy.cs
@@ -6,39 +6,57 @@ public class PotatoGuy : MonoBehaviour {
 
     //members
     public float speed = 1.5f;
+    public Sprite[] array;
+    private SpriteRenderer sr;
 
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 	
 	// Update is called once per frame
      void Update ()
      {
         Movement();
+        Schieten();
      }
 
+    //beweging voor potatoguy, deze moet nog aangepast worden
     void Movement()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
-            return;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
-            return;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += Vector3.up * speed * Time.deltaTime;
-            return;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += Vector3.down * speed * Time.deltaTime;
-            return;
+        }
+    }
+
+    void Schieten()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Schietanimatie(); 
+        }
+    }
+
+    void Schietanimatie()
+    {
+        int teller = 0;
+        for (teller; teller < 2; teller++)
+        {
+            sr.sprite = array[teller];
         }
     }
  }
